d3b67e414cf40441371c1c77b71b7187cd05723a
wayne <juchuang123@gmail.com>
2021-01-12 23:09:58 +0900

    サインアップの実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  skip_before_action :login_required
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      log_in @user
+      redirect_to memos_url, notice: "#{@user.name}さんようこそ！"
+    else
+      render :new
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+end
