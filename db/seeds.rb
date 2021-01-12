e63810ffd0bd0cb3103e9d82c9e4700e8f81add3
wayne <juchuang123@gmail.com>
2021-01-12 22:30:15 +0900

    seedの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.find_or_create_by!(email: 'admin@email.com') do |user|
+  user.name = 'admin'
+  user.admin = true
+  user.password = 'password'
+  user.password_confirmation = 'password'
+end
+
+=begin
+(1..50).each do |i|
+  User.find_or_create_by!(email: "foo#{i}@email.com") do |user|
+    user.name = "user"
+    user.admin = false
+    user.password = "p"
+    user.password_confirmation = "p"
+  end
+end
+=end
+
+(1..50).each do |i|
+  Memo.create!(
+    title: "title#{i}",
+    content: "content#{i}",
+    user_id: 1
+  )
+end
